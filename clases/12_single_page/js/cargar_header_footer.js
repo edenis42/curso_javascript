9ae18922838155560868e5fba399680a75b8741a
Eduardo Denis <edenise@frd.utn.edu.ar>
2022-11-01 19:50:32 -0300

    ajax

---
+++ clases/12_single_page/js/cargar_header_footer.js
@@ -0,0 +1,29 @@
+var peticionHeader = new XMLHttpRequest();
+
+peticionHeader.addEventListener('readystatechange', function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // codigo
+        console.log(this.responseText);
+        document.querySelector("header").innerHTML = this.responseText;
+
+    }
+});
+
+peticionHeader.open("GET", "./layout/header.html", true);
+peticionHeader.setRequestHeader("Content-type", "html");
+peticionHeader.send();
+
+var peticionFooter = new XMLHttpRequest();
+
+peticionFooter.addEventListener('readystatechange', function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // codigo
+        console.log(this.responseText);
+        document.querySelector("footer").innerHTML = this.responseText;
+
+    }
+});
+
+peticionFooter.open("GET", "./layout/footer.html", true);
+peticionFooter.setRequestHeader("Content-type", "html");
+peticionFooter.send();
